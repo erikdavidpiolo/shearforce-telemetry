fa4b44fc214e0babc15860eea105c173678b2448
Erik David Piolo <pioloe@uci.edu>
2026-04-10 00:29:31 -0700

    frick u kaeden

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,15 @@
 #include "BluetoothSerial.h"
 
 HardwareSerial SerialESC1(2);
-#define RXD2 16
+#define RXD2 16 //esp32 -> rx2
 
 HardwareSerial SerialESC2(1);
-#define RXD1 18  // Change to your actual pin
+#define RXD1 18  // Change to your actual pin -> D18
+
+//dont forget common gnd! gnd-> gnd
 
 BluetoothSerial SerialBT;
-#define BT_DEVICE_NAME "ESC_Telemetry"
+#define BT_DEVICE_NAME "ESC_Telemetry" //changes name of the bt device!
 
 struct ESC_Data {
   uint8_t  temp;
@@ -22,6 +24,8 @@ struct ESC_Data {
 ESC_Data tlm1, tlm2;
 uint8_t buf1[10], buf2[10];
 
+
+//this stuff checks that you have everything before sending it out -refer to kiss telemetry pdf file
 uint8_t update_crc8(uint8_t crc, uint8_t crc_seed) {
   uint8_t crc_u = crc ^ crc_seed;
   for (int i = 0; i < 8; i++) {
@@ -35,6 +39,8 @@ uint8_t get_crc8(uint8_t *buf, uint8_t size) {
   for (uint8_t i = 0; i < size; i++) crc = update_crc8(buf[i], crc);
   return crc;
 }
+//end of thing
+
 
 void parseESC(uint8_t *buffer, ESC_Data &tlm) {
   if (get_crc8(buffer, 9) == buffer[9]) {
@@ -47,6 +53,8 @@ void parseESC(uint8_t *buffer, ESC_Data &tlm) {
   }
 }
 
+
+//this is for debuggging
 void broadcastPrint(const char* msg) {
   Serial.print(msg);
   if (SerialBT.connected()) {
@@ -54,6 +62,8 @@ void broadcastPrint(const char* msg) {
   }
 }
 
+
+//setup once
 void setup() {
   Serial.begin(115200);
   SerialESC1.begin(115200, SERIAL_8N1, RXD2, -1, false);
@@ -64,14 +74,17 @@ void setup() {
   tlm1.valid = false;
   tlm2.valid = false;
 
-  Serial.println("Dual ESC Telemetry Online");
+  Serial.println("Dual ESC Telemetry Online"); //change if u want
   Serial.printf("Bluetooth device ready: \"%s\"\n", BT_DEVICE_NAME);
 }
 
+
+//main loop
 void loop() {
+  //checks if the telemetry from esc is actually sending sutff
   if (SerialESC1.available() >= 10) {
     SerialESC1.readBytes(buf1, 10);
-    parseESC(buf1, tlm1);
+    parseESC(buf1, tlm1);    // <--- 
   }
 
   if (SerialESC2.available() >= 10) {
@@ -79,9 +92,15 @@ void loop() {
     parseESC(buf2, tlm2);
   }
 
+//dont change lol
+
+
+//serial studio: just set it up to dis
+//first 5 values coorespond to esc1
+//next 5 values is esc2
   if (tlm1.valid && tlm2.valid) {
     char msg[128];
-    snprintf(msg, sizeof(msg), "/*%d,%.2f,%.2f,%d,%d,%d,%.2f,%.2f,%d,%d*/\n",
+    snprintf(msg, sizeof(msg), "%d,%.2f,%.2f,%d,%d,%d,%.2f,%.2f,%d,%d\n",
              tlm1.temp,
              tlm1.voltage / 100.0,
              tlm1.current / 100.0,
@@ -93,7 +112,8 @@ void loop() {
              tlm2.consumption,
              tlm2.erpm * 100);
 
-    broadcastPrint(msg);
+    broadcastPrint(msg); 
+    //bluetooth send
     tlm1.valid = false;
     tlm2.valid = false;
   }
